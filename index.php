e153117e8dfff00437b611a9a92fe9b10c0053b1
M033A <igormotl@gmail.com>
2018-11-16 12:48:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta name="description" content="Doni Blagojevic is Database Administrator and Developer, Business Intelligence
+Developer, and Data Warehouse Developer">
+       
+        <link rel="stylesheet" type ="text/css" href="vendors/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+        <link href="https://unpkg.com/ionicons@4.4.6/dist/css/ionicons.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+        
+        <title>DBdevelopment</title>
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="resources/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="resources/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="resources/favicons/favicon-16x16.png">
+        <link rel="manifest" href="resources/favicons/site.webmanifest">
+        <link rel="mask-icon" href="resources/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="resources/favicons/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="resources/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+        
+    </head>
+    <body>
+       <header>
+          <nav>
+              <div class="row">
+                  <img src="resources/img/DBlogoWhite.png" alt="DBD logo" class="logo">
+                  <img src="resources/img/DBlogoDark.png" alt="DBD logo" class="logo-dark">
+                  <ul class="main-nav">
+                      <li><a href="#skills">Skills</a></li>
+                      <li><a href="#biography">Biography</a></li>
+                      <li><a href="#contact">Contact</a></li>
+                  </ul>
+                  <a class="mobile-nav-icon js--nav-icon"><i class="icon ion-md-menu"></i></a>
+              </div>
+          </nav>
+           <div class="hero-text-box">
+               <h1>Every database<br>needs a good character</h1>
+               <a class="btn btn-full js--btn-full" href="#">Let's talk</a>
+               <a class="btn btn-ghost js--btn-ghost" href="#">More</a>
+           </div>
+           
+       </header>
+       
+       <section class="section-skills" id="skills">
+           <div class="row">
+               <h2>Skills</h2>
+               <p class="long-copy">
+                   Database Administrator and Developer, Business Intelligence
+                Developer, and Data Warehouse Developer
+                 SQL &mdash; business Intelligence Developer, Microsoft Certified Solutions Expert MCSE: Data Management and Analytics &mdash;
+
+Developing Microsoft SQL Server Databases
+               </p>
+           </div>
+           <div class="row skills-with-icons js--fadein">
+               <div class="col span-1-of-4 box">
+                  <i class="icon ion-md-construct icon-big"></i>
+                   <h3>Development</h3>
+                   <p>and improvement of ad hoc reports (SQL and T-SQL). Development and improvement of data warehouse. Administration of MS SQL and MySQL databases. Development, maintenance, control, and testing of ETL processes.
+                  </p>
+               </div>
+               <div class="col span-1-of-4 box">
+                    <i class="icon ion-md-code-download icon-big"></i>
+                   <h3>Importing</h3>
+                   <p>data from flat files and exporting data to flat files. Preparation of files for importing into SalesForce. Development, design, improvement, and testing of reports. Communication with users, definition of requests, planning and development of
+                    reports.
+                  </p>
+               </div>
+               <div class="col span-1-of-4 box">
+                  <i class="icon ion-md-cube icon-big"></i>
+                   <h3>Implementation</h3>
+                   <p>and development of SSIS, SSAS projects. Adjusting queries to be used with SSRS projects. Data retrieval using complex SQL queries. Data mining.
+                  </p>
+               </div>
+               <div class="col span-1-of-4 box">
+                  <i class="icon ion-md-stats icon-big"></i>
+                   <h3>Automation</h3>
+                   <p>of reports with Power BI, Tableau, and Power Query. Automation of Excel based reports with VBA. Developing MDX and DAX queries.
+                  </p>
+               </div>
+           </div>
+       </section>
+       
+       <section class="section-software">
+           <ul class="software-showcase clearfix">
+               <li>
+                   <figure class="software-logo">
+                       <img src="resources/img/powerBi.png" alt="Power BI logo">
+                   </figure>
+               </li>
+               <li>
+                   <figure class="software-logo">
+                       <img src="resources/img/MySQL.png" alt="MySQL logo">
+                   </figure>
+               </li>
+               <li>
+                   <figure class="software-logo">
+                       <img src="resources/img/SQLserver.png" alt="Microsoft SQL logo">
+                   </figure>
+               </li>
+               <li>
+                   <figure class="software-logo">
+                       <img src="resources/img/VisualStudio.png" alt="VisualStudio logo">
+                   </figure>
+               </li>
+           </ul>
+       </section>
+       
+       <section class="section-bio" id="biography">
+           <div class="row">
+               <h2>Biography</h2>
+           </div>
+           <div class="row">
+               <div class="col span-1-of-3 bio-box">
+                   <img src="resources/img/dsc_00802.jpg" alt="Doni Blagojevic photo" class="db-photo">
+               </div>
+               <div class="col span-2-of-3 bio-box">
+                  <h3>Doni Blagojevic</h3>
+                  <h4>Prijedor, Bosnia and Herzegovina</h4>
+                  <h3>Database Administrator and Developer,<br>Business Intelligence Developer,<br>and Data Warehouse Developer</h3>
+                   <p><strong>Education:</strong></p>
+
+                    <p>1997 &mdash; 2003 <br> 
+                    Diploma Engineer of Forestry (Faculty of Forestry / University of Belgrade, MSc)</p>
+
+                    <p>2016 <br>
+                    Microsoft Certified Solutions Associate MCSA SQL Server 2012/2014 (Microsoft Academy)</p>
+
+                    <p>2017 &mdash; ongoing <br>
+                    Microsoft Certified Solutions Expert MCSE: Data Management and Analytics &mdash; Developing Microsoft SQL Server Databases (Microsoft Academy)</p>
+
+
+                    <p><strong>Working experience:</strong></p>
+
+                    <p>2004 &mdash; 2009 <br>
+                   Teaching Assistant <br>
+                    Head of the department for Forest Economics and Organization,
+                    <a class="bio-links" href="http://sf.unibl.org" target="_blank">Faculty of Forestry</a> / University of Banja Luka, Stepe Stepanovica 75a 78000 Banja Luka, Bosnia and Herzegovina</p>
+
+                    <p>June 2009 &mdash; March 2014 <br>
+                    Project Vice-coordinator
+                    <a class="bio-links" href="https://www.efi.int/" target="_blank">European Forest Institute;</a> Yliopistokatu 6; 80100 Joensuu; Finland</p>
+
+                    <p>October 2014 &mdash; 2016 <br>
+                    Freelancer (Database Administrator and Business Intelligence Developer)
+                    Various employers</p>
+
+                    <p>2016 &mdash; ongoing <br>
+                     SQL / Business Intelligence Developer
+                    <a class="bio-links" href="http://www.voxcode.tech/" target="_blank">Vox Code</a>, 11000 Belgrade, Serbia
+                   </p>
+               </div>
+           </div>
+       </section>
+       
+       <section class="section-form" id="contact">
+           <div class="row">
+               <h2>Drop me a line</h2>
+           </div>
+           
+           <?php
+           if($_GET['success'] == 1) {
+               echo "<div class=\"form-message success\">Thank you! Your message has been sent.</div>"
+           }
+           
+            if($_GET['success'] == -1) {
+               echo "<div class=\"form-message error\">Oops!!! Something went wrong. Please try again.</div>"
+           } 
+           
+           
+           ?>
+           
+           <div class="row">
+               <form action="mailer-new.php" method="post" class="contact-form">
+                   <div class="row">
+                       <div class="col span-1-of-3">
+                           <label for="name">Name</label>
+                       </div>
+                       <div class="col span-2-of-3">
+                           <input type="text" name="name" id="name" placeholder="Your name" required>
+                       </div>
+                   </div>
+                   <div class="row">
+                       <div class="col span-1-of-3">
+                           <label for="email">Email</label>
+                       </div>
+                       <div class="col span-2-of-3">
+                           <input type="email" name="email" id="email" placeholder="Your email" required>
+                       </div>
+                   </div>
+                   <div class="row">
+                       <div class="col span-1-of-3">
+                           <label for="message">Message</label>
+                       </div>
+                       <div class="col span-2-of-3">
+                           <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message"></textarea>
+                       </div>
+                   </div>
+                   <div class="row">
+                       <div class="col span-1-of-3">
+                           <label>&nbsp;</label>
+                       </div>
+                       <div class="col span-2-of-3">
+                           <input type="submit" value="Send it!">
+                       </div>
+                   </div>
+               </form>
+           </div>
+       </section>
+       
+       <footer>
+           <div class="row">
+               <ul class="social-nav">
+                   <li><a href="#"><i class="icon ion-logo-facebook icon-small"></i></a></li>
+                   <li><a href="#"><i class="icon ion-logo-twitter icon-small"></i></a></li>
+                   <li><a href="#"><i class="icon ion-logo-instagram icon-small"></i></a></li>
+               </ul>
+           </div>
+           <div class="row footer-text">
+               <p>
+                   Copyright &copy; 2018 by Doni Blagojevic
+               </p>
+               <p class="m033a">
+                   Design by M033A
+               </p>
+               
+           </div>
+       </footer>
+     
+        
+           
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/respond@0.9.0/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/selectivizr@1.0.3/selectivizr.min.js"></script>
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/scripts.js"></script>   
+    </body>
+</html>
+
